3366fd26e485b3d7273d7dd673ae5c1b239b5b60
kittitachthi <kittitachthi@gmail.com>
2022-08-16 18:44:17 +0700

    Add new Wellcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -134,6 +134,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Wellcome to the Application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -142,6 +146,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
